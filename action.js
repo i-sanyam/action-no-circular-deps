c841218203865acb9caa578f1d23fc03a72563b2
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2022-12-25 17:55:01 +0530

    add basic input baseFilePath

--- action.js
+++ action.js
@@ -1,5 +1,14 @@
 async function run() {
   console.log('Hello world!');
+  try {
+    const baseFilePath = core.getInput('baseFilePath');
+    if (!baseFilePath) {
+      core.info('baseFilePath not specified');
+    }
+    core.info(`baseFilePath:: ${baseFilePath}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();

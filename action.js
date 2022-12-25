f2304c55c21142a32a0659d0ca6347c001681f5a
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2022-12-25 18:02:00 +0530

    remove console

--- action.js
+++ action.js
@@ -1,7 +1,6 @@
 import * as core from '@actions/core';
 
 async function run() {
-  console.log('Hello world!');
   try {
     const baseFilePath = core.getInput('baseFilePath');
     if (!baseFilePath) {
